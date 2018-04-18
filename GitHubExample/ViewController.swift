705398c594339cf63f94eb955cbe6c0d51a0cbe3
Levi Newman <lnewman1257@jh-mba158-17.d214.org>
2018-04-18 14:44:04 -0500

    Initial Commit

---
+++ GitHubExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubExample
+//
+//  Created by Levi Newman on 4/18/18.
+//  Copyright © 2018 period8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
