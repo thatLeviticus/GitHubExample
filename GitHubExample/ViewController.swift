a2013d92717e1b975a44b36d7c5525886e99d190
Levi Newman <lnewman1257@jh-mba158-17.d214.org>
2018-04-18 14:55:55 -0500

    WOW....added console prints added BUTTON

--- GitHubExample/ViewController.swift
+++ GitHubExample/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+            print("button clicked - GitHub Tutorial")
+        }
+    
 
 
 }
